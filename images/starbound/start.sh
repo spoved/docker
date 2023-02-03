cd019b2f4bae6da87dfdd4fca7d9baff9669ea22
Holden Omans <holden.omans@gmail.com>
2023-02-03 07:52:12 -0500

    minor update

--- images/starbound/start.sh
+++ images/starbound/start.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/sh
+set -x
 
 download_and_install() {
-  set -e
+  # set -e
   cd /tmp
+
+  apt-get update
+  apt-get install -y wget 7zip
+
   echo "Downloading starbound..."
-  wget ${STARBOUND_INSTALLER}
+  wget "${STARBOUND_INSTALLER}" -O starbound.tgz
+  if [ ! -e starbound.tgz ]; then
+    echo "Unable to download installer"
+    exit 1
+  fi
   echo "Extracting..."
-  unzip starbound_1_4_4_34261.sh
+  7z x starbound.tgz
+  if [ ! -e ./data/noarch ]; then
+    echo "Unable to extract installer"
+    exit 1
+  fi
   echo "Installing..."
   mv ./data/noarch/* /starbound
   echo "Cleaning up..."
@@ -31,4 +44,32 @@ else
 fi
 
 chmod +x starbound_server
+
+if [ ! -z "${STARBOUND_CONFIG}" ]; then
+  if [ -e ${STARBOUND_CONFIG} ]; then
+    # /starbound/game/storage/starbound_server.config
+    # cp ${STARBOUND_CONFIG} ../storage/starbound_server.config
+
+    echo "Linking starbound config"
+    my_link=../storage/starbound_server.config
+
+    if [ -L ${my_link} ]; then
+      if [ -e ${my_link} ]; then
+        echo "Good link"
+      else
+        echo "Broken link"
+        rm ${my_link}
+        ln -s ${STARBOUND_CONFIG} ${my_link}
+      fi
+    elif [ -e ${my_link} ]; then
+      echo "Not a link"
+      rm ${my_link}
+      ln -s ${STARBOUND_CONFIG} ${my_link}
+    else
+      echo "Missing"
+      ln -s ${STARBOUND_CONFIG} ${my_link}
+    fi
+  fi
+fi
+
 ./starbound_server
